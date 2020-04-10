f75172ffc59a46b492a152f15823bcbe96f1c8e2
Sam Rivera <samrivera.contact@gmail.com>
2020-04-09 19:24:53 -0500

    Add scoring process function
    Given a selection from the user pass the dice and the scorepad object
    to the function to calculate the score and update the scorepad.

--- pkg/gameprocessing/scoring.py
+++ pkg/gameprocessing/scoring.py
@@ -12,15 +12,25 @@ fixed_scores = {'score_full_house': 25,
                 }
 
 
-def upper_section_score(die_value, final_dice):
+def upper_section_score(die_value,
+                        final_dice,
+                        scorepad,
+                        ):
     """Evaluate dice from a roll for a given value and multiply the count
     by that value. E.g. for a die value of 3 and a count of two dice with
     that value the total score is 6 (die value of 3 times a count of two)
     """
-    return final_dice.count(die_value) * die_value
+
+    if die_value in range[1,7]:
+        return final_dice.count(die_value) * die_value
+    else:
+        return 0
 
 
-def score_number_of_a_kind(final_dice, kind_type):
+def score_number_of_a_kind(final_dice,
+                           kind_type,
+                           scorepad,
+                           ):
 
     final_dice = sorted(final_dice)
 
@@ -43,7 +53,9 @@ def score_number_of_a_kind(final_dice, kind_type):
         return 0
 
 
-def score_full_house(final_dice):
+def score_full_house(final_dice,
+                     scorepad,
+                     ):
 
     final_dice = sorted(final_dice)
 
@@ -59,7 +71,9 @@ def score_full_house(final_dice):
         return 0
 
 
-def score_small_straight(final_dice):
+def score_small_straight(final_dice,
+                         scorepad,
+                         ):
     final_dice = sorted(final_dice)
     final_dice = final_dice[0:4]
 
@@ -75,7 +89,9 @@ def score_small_straight(final_dice):
         return 0
 
 
-def score_large_straight(final_dice):
+def score_large_straight(final_dice,
+                         scorepad,
+                         ):
     if sorted(final_dice) in [[1, 2, 3, 4, 5],
                               [2, 3, 4, 5, 6],
                               ]:
@@ -84,15 +100,116 @@ def score_large_straight(final_dice):
         return 0
 
 
-def score_chance(final_dice):
+def score_chance(final_dice,
+                 scorepad,
+                 ):
     return sum(final_dice)
 
 
-def process_category_selection(final_dice, scorepad):
+def validate_bonus(scorepad):  #  Need to add code to validate
+    """Need to check if bonus scoring is valid.
+    Bonus is awarded when there has already been one five of a kind and there
+    are still at least one category that has not yet been scored.
+    """
     pass
 
 
+def process_category_selection(final_dice, selection, scorepad):
+    """Based on selection from user calculate and populate scorepad object with
+    score and return object"""
+
+    # Update score for selection of 1's through 6's
+    if selection in ['1',
+                     '2',
+                     '3',
+                     '4',
+                     '5',
+                     '6',
+                     ]:
+
+        upper_score = upper_section_score(int(selection),
+                                          final_dice,
+                                          scorepad,
+                                          )
+
+        if selection == '1':
+            scorepad.upper_ones += upper_score
+            scorepad.track_ones = 1
+        elif selection == '2':
+            scorepad.upper_twos += upper_score
+            scorepad.track_twos = 1
+        elif selection == '3':
+            scorepad.upper_threes += upper_score
+            scorepad.track_threes = 1
+        elif selection == '4':
+            scorepad.upper_fours += upper_score
+            scorepad.track_fours = 1
+        elif selection == '5':
+            scorepad.upper_fives += upper_score
+            scorepad.track_fives = 1
+        elif selection == '6':
+            scorepad.upper_sixes += upper_score
+            scorepad.track_sixes = 1
+        else:
+            print('*** ERROR - INVALID UPPER SECTION SELECTION ***')
+
+    if selection == 'A':  # Three of a Kind
+        scorepad.lower_kind_three_of = score_number_of_a_kind(final_dice,
+                                                              3,
+                                                              scorepad,
+                                                              )
+        scorepad.track_kind_three_of = 1
+
+    if selection == 'B':  # Four of a Kind
+        scorepad.lower_kind_four_of = score_number_of_a_kind(final_dice,
+                                                             4,
+                                                             scorepad,
+                                                             )
+        scorepad.track_kind_four_of = 1
+
+    if selection == 'C':  # Full House
+        scorepad.lower_full_house = score_full_house(final_dice,
+                                                     scorepad,
+                                                     )
+        scorepad.track_full_house = 1
+
+    if selection == 'D':  # Small Straight
+        scorepad.lower_straight_small = score_small_straight(final_dice,
+                                                             scorepad,
+                                                             )
+        scorepad.track_straight_small = 1
+
+    if selection == 'E':  # Large Straight
+        scorepad.lower_straight_large = score_small_straight(final_dice,
+                                                             scorepad,
+                                                             )
+        scorepad.track_straight_large = 1
+
+    if selection == 'F':  # Five of a Kind
+        scorepad.lower_kind_five_of = score_number_of_a_kind(final_dice,
+                                                             5,
+                                                             scorepad,
+                                                             )
+        scorepad.track_kind_five_of = 1
+
+    if selection == 'G':  # Total all dice
+        scorepad.lower_all_dice = score_chance(final_dice,
+                                               scorepad,
+                                               )
+        scorepad.track_all_dice = 1
+
+    if selection == 'H':  # Bonus for additional Five of a Kind
+        if scorepad.track_kind_five_of == 1 and validate_bonus(scorepad):
+            scorepad.lower_bonus += 100
+            scorepad.track_bonus += 1
+
+    return scorepad
+
+
 if __name__ == '__main__':
+
+    # Add scorepad object for testing below. Need to add to arguments as well
+    #  TODO
 
     pass_this_list = [1, 1, 3, 4, 1, 2, 3, 3, 3,]
     die_value = int(input('Enter a value to count in the list: '))
